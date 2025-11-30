8cf9efc5dec7588de2b7122f87618d7d4715a33f
Abdullah <abdullahdeveloper843@gmail.com>
2025-11-30 14:48:09 +0500

    The whole card is now a button that can be clicked on to get to the product desccription page.

--- client/src/components/Global/Card.jsx
+++ client/src/components/Global/Card.jsx
@@ -14,53 +14,55 @@ const Cards = ({ product }) => {
   const imageUrl = product.image;
 
   return (
-    <div
-      key={product._id}
-      className="group bg-gradient-to-b bg-black rounded-lg overflow-hidden border border-white/10 hover:border-white/30 transition-all duration-300 shadow-lg max-w-[200px]"
-      onClick={() => {
-        sendProductData(); // Call the function to send product data to Redux store
-      }}
-    >
-      {/* Product Image */}
-      <div className="aspect-[3/4] overflow-hidden">
-        <img
-          src={imageUrl}
-          alt={product.name}
-          className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-500"
-        />
-      </div>
-
-      {/* Product Info */}
-      <div className="p-2">
-        {/* Title and Rating */}
-        <div className="flex justify-between items-start mb-1">
-          <h3 className="text-white/90 font-medium text-sm truncate max-w-[120px]">
-            {product.name}
-          </h3>
+    <Link to={`/shop/:${product._id}`}>
+      <div
+        key={product._id}
+        className="group bg-gradient-to-b bg-black rounded-lg overflow-hidden border border-white/10 hover:border-white/30 transition-all duration-300 shadow-lg max-w-[200px]  cursor-pointer"
+        onClick={() => {
+          sendProductData(); // Call the function to send product data to Redux store
+        }}
+      >
+        {/* Product Image */}
+        <div className="aspect-[3/4] overflow-hidden">
+          <img
+            src={imageUrl}
+            alt={product.name}
+            className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-500"
+          />
         </div>
 
-        {/* Category */}
-        <p className="text-white/50 text-xs mb-1.5">{product.category}</p>
+        {/* Product Info */}
+        <div className="p-2">
+          {/* Title and Rating */}
+          <div className="flex justify-between items-start mb-1">
+            <h3 className="text-white/90 font-medium text-sm truncate max-w-[120px]">
+              {product.name}
+            </h3>
+          </div>
 
-        {/* Price and Add to Cart */}
-        <div className="flex justify-between items-center">
-          <p className="text-black p-1 rounded-md font-bold text-xs bg-gray-300">
-            {product.price} $
-          </p>
-          {/* Add to Cart Button with product._id as the url parameter */}
-          <Link to={`/shop/:${product._id}`}>
-            <button
-              className="cursor-pointer p-1 rounded-md bg-gray-300 hover:bg-white text-black text-xs font-medium transition-all duration-300"
-              onClick={() => {
-                sendProductData(); // Call the function to send product data to Redux store
-              }}
-            >
-              <img src={assets.bag} alt="add to cart" className="w-5" />
-            </button>
-          </Link>
+          {/* Category */}
+          <p className="text-white/50 text-xs mb-1.5">{product.category}</p>
+
+          {/* Price and Add to Cart */}
+          <div className="flex justify-between items-center">
+            <p className="text-black p-1 rounded-md font-bold text-xs bg-gray-300">
+              {product.price} $
+            </p>
+            {/* Add to Cart Button with product._id as the url parameter */}
+            <Link to={`/shop/:${product._id}`}>
+              <button
+                className="cursor-pointer p-1 rounded-md bg-gray-300 hover:bg-white text-black text-xs font-medium transition-all duration-300"
+                onClick={() => {
+                  sendProductData(); // Call the function to send product data to Redux store
+                }}
+              >
+                <img src={assets.bag} alt="add to cart" className="w-5" />
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
