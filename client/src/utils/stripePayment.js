391d29cbdd759f4841abbf8c2471558237220e1c
Abdullah <abdullahdeveloper843@gmail.com>
2025-12-18 04:35:19 +0500

    Minor Changes.

--- client/src/utils/stripePayment.js
+++ client/src/utils/stripePayment.js
@@ -7,10 +7,10 @@ const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
 export const processStripePayment = async (paymentData) => {
   try {
     const stripe = await stripePromise;
-
+    // This creates a checkout session on the server and or that provides the required data to start the stripe checkout process
     const res = await api.createCheckOutSession(paymentData);
-
     const { sessionId } = res.data;
+    // After the checkout session is created (in stripe.js (backend)), redirect to stripe checkout page
     await stripe.redirectToCheckout({ sessionId });
   } catch (error) {
     console.error("Error processing Stripe payment:", error);
