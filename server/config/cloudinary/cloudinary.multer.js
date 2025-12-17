6a8187b6f8a7d2126ea2681b9af7c4a42f63c539
Abdullah <abdullahdeveloper843@gmail.com>
2025-12-18 03:33:33 +0500

    Minor changes.

--- server/config/cloudinary/cloudinary.multer.js
+++ server/config/cloudinary/cloudinary.multer.js
@@ -2,10 +2,12 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("./cloudinary.config.js");
 
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
+  cloudinary: cloudinary, // This is all the config
   params: {
     folder: "anime-alley-products",
     allowed_formats: ["jpg", "jpeg", "png", "webp"],
+    // If the image is larger than the specified dimensions then crop: "limit"
+    //  will resize them to fit in 800 x 800 without affecting the aspect ratio
     transformation: [{ width: 800, height: 800, crop: "limit" }],
   },
 });
