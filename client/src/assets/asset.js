bab3011f6f6245522356722a2860b2e381205c40
Abdullah <abdullahdeveloper843@gmail.com>
2026-05-02 10:38:34 +0500

    Fixed a minor import issue.

--- client/src/assets/asset.js
+++ client/src/assets/asset.js
@@ -3,7 +3,7 @@ import search from "./img/search.svg";
 import banner from "./img/homePics/banner.webp";
 import menu from "./img/menu.webp";
 import genos from "./img/genos.jpeg";
-import goku from "./img/goku.png";
+
 import comicBackground from "./img/homePics/comicBackground.webp";
 import halloweenBackground from "./img/homePics/halloweenBackground.webp";
 import jin from "./img/homePics/jin.webp";
@@ -63,7 +63,7 @@ const assets = {
   banner,
   menu,
   genos,
-  goku,
+
   comicBackground,
   halloweenBackground,
   jin,
